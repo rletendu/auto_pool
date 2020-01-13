1b7d1256f02a426ff33cfd8b2e4ccec5cf332621
raphael letendu <raphael.letendu@microchip.com>
2020-01-13 18:06:56 +0100

    More wip

--- src/parameters.cpp
+++ src/parameters.cpp
@@ -1,13 +1,11 @@
+#include "parameters.h"
 #include <FS.h>                   //this needs to be first, or it all crashes and burns...
 #include <SPIFFS.h>
 #include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
 
 
-char mqtt_server[50] = "xxx.cloudmqtt.com";
-char mqtt_port[50]  =       "12345";
-char mqtt_user[50]   =      "mqtt_user";
-char mqtt_pass[50]   =      "mqtt_pass";
-char humidity_topic[50] =   "sensor/humidity";
+struct ParametersStruture parameters;
+
 
 
 void parameters_read_json(void)
@@ -25,21 +23,16 @@ void parameters_read_json(void)
         size_t size = configFile.size();
         // Allocate a buffer to store contents of the file.
         std::unique_ptr<char[]> buf(new char[size]);
-
         configFile.readBytes(buf.get(), size);
         DynamicJsonBuffer jsonBuffer;
         JsonObject& json = jsonBuffer.parseObject(buf.get());
         json.printTo(Serial);
         if (json.success()) {
           Serial.println("\nparsed json");
-          strcpy(mqtt_server, json["mqtt_server"]);
-          strcpy(mqtt_port, json["mqtt_port"]);
-          strcpy(mqtt_user, json["mqtt_user"]);
-          strcpy(mqtt_pass, json["mqtt_pass"]);
-          port = json["mqtt_port"];
-          Serial.print("Port : ");
-          Serial.println(port);
-
+          strcpy(parameters.mqtt_server, json["mqtt_server"]);
+          parameters.mqtt_port = json["mqtt_port"];
+          strcpy(parameters.mqtt_user, json["mqtt_user"]);
+          strcpy(parameters.mqtt_pass, json["mqtt_pass"]);
         } else {
           Serial.println("failed to load json config");
         }
@@ -56,10 +49,10 @@ void parameters_write_json(void)
   Serial.println("saving config");
   DynamicJsonBuffer jsonBuffer;
   JsonObject& json = jsonBuffer.createObject();
-  json["mqtt_server"] = mqtt_server;
-  json["mqtt_port"] = mqtt_port;
-  json["mqtt_user"] = mqtt_user;
-  json["mqtt_pass"] = mqtt_pass;
+  json["mqtt_server"] = parameters.mqtt_server;
+  json["mqtt_port"] = parameters.mqtt_port;
+  json["mqtt_user"] = parameters.mqtt_user;
+  json["mqtt_pass"] = parameters.mqtt_pass;
 
   File configFile = SPIFFS.open("/config.json", "w");
   if (!configFile) {
--- src/parameters.h
+++ src/parameters.h
@@ -2,14 +2,16 @@
 
 #define PARAM_MAX_STR 50
 
-struct Parameters
+struct ParametersStruture
 {
+	char mqtt_server[PARAM_MAX_STR];
 	uint16_t mqtt_port;
 	char mqtt_user[PARAM_MAX_STR];
 	char mqtt_pass[PARAM_MAX_STR];
 	char humidity_topic[PARAM_MAX_STR];
 };
 
+extern struct ParametersStruture parameters;
 
 extern char *mqtt_server;
 extern char *mqtt_port;
@@ -21,3 +23,7 @@ char mqtt_user[50]   =      "mqtt_user";
 char mqtt_pass[50]   =      "mqtt_pass";
 char humidity_topic[50] =   "sensor/humidity";
 */
+
+void parameters_read_json(void);
+void parameters_write_json(void);
+void parameters_format(void);
